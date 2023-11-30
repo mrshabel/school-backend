83a65c7c18ee03aef9d1b119e63d82f3fb06d03c
mrshabel <sgumah001@st.ug.edu.gh>
2023-11-30 16:56:37 +0000

    add image upload middleware

--- middleware/student.js
+++ middleware/student.js
@@ -1,3 +1,36 @@
+const multer = require("multer");
+const AppError = require("../utils/appError");
+
+// student photo upload
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/img/student");
+  },
+  filename: (req, file, cb) => {
+    //name-class-currentTime.ext
+    let { name, class: studentClass } = req.body;
+    name = name.split(" ").join("");
+    studentClass = studentClass.split(" ").join("");
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `${name}-${studentClass}-${Date.now()}.${ext}`);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("Only images are accepted", 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadStudentPhoto = upload.single("displayImage");
+
 exports.setStudentParents = async (req, res, next) => {
   req.body = {
     name: req.body.name,
